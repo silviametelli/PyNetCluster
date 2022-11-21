58564f7ae1411ff8dfa7c6ced85fab50cd2378c6
Silvia Metelli <silvia.metelli1@gmail.com>
2022-11-21 15:59:49 +0100

    first commit

--- src/setup.py
+++ src/setup.py
@@ -3,8 +3,15 @@ from setuptools import setup
 setup(
     name="pyclusterseql",
     version="0.0.1",
-    description="batch and sequential agglomerative clustering",
-    py_modules=["seql_clustering"],
+    packages=["net_cluster"],
+    #description="batch and sequential agglomerative clustering",
+    #py_modules=["seql_clustering"],
+    install_requires = [
+        'numpy',
+        'sys',
+        'math',
+        'operator',
+    ],
     package_dir={'': 'src'},
 )
 
